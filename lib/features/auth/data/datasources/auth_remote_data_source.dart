0a4ac3f7a88db9d46176fbb9398a1a40cd46be3a
AnshChugh <Anshchugh20@GMAil.com>
2024-05-28 22:23:12 +0530

    created auth feature components

---
+++ lib/features/auth/data/datasources/auth_remote_data_source.dart
@@ -0,0 +1,36 @@
+import 'package:blog_flutter/core/error/exceptions.dart';
+import "package:firebase_auth/firebase_auth.dart";
+
+abstract interface class AuthRemoteDataSource {
+  Future<String> signUpWithEmailPassword(
+      {required String name, required String email, required String password});
+  Future<String> loginWithEmailPassword(
+      {required String email, required String password});
+}
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  final FirebaseAuth firebaseAuthInstance;
+  AuthRemoteDataSourceImpl(this.firebaseAuthInstance);
+  @override
+  Future<String> signUpWithEmailPassword(
+      {required String name,
+      required String email,
+      required String password}) async {
+    try {
+      final userCredentials = await firebaseAuthInstance
+          .createUserWithEmailAndPassword(email: email, password: password);
+      await userCredentials.user?.updateDisplayName(name);
+      await userCredentials.user?.reload();
+      return userCredentials.user!.uid;
+    } catch (e) {
+      throw ServerException(e.toString());
+    }
+  }
+
+  @override
+  Future<String> loginWithEmailPassword(
+      {required String email, required String password}) {
+    //TODO: Implement LoginWithEmailPassword
+    throw UnimplementedError();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:blog_flutter/core/theme/theme.dart';
 import 'package:blog_flutter/features/auth/presentation/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:blog_flutter/firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
-}
+} 
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
